b21681537998905f19275b79da3e9a0e123c1a7e
juan támara <t4mara95@gmail.com>
2025-06-18 12:17:40 -0500

    modificacion de la clase para la implementacion de la libreria swag en la clase pomxml

--- src/main/java/aulas/back/config/CorsConfig.java
+++ src/main/java/aulas/back/config/CorsConfig.java
@@ -6,14 +6,17 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig{
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3001")
+                        .allowedOrigins(
+                                "http://localhost:3001",
+                                "https://back-aulas-production.up.railway.app"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
